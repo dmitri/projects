b53ebeab22f171e4568fe22804ad72d91d44a39f
Steve Zimmerman <szimmerman@berklee.edu>
2014-11-09 07:34:25 -0500

    trying includes

---
+++ js/property_proto.js
@@ -0,0 +1,10 @@
+/* simple functions for the prototype */
+
+$( document ).ready(function() {
+  // load the includes
+  $( "#carousel" ).load( "carousel.html" );
+  // pause the carousel
+  //$('.carousel').pause();
+  
+  
+});
