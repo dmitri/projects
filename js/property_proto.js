970673c329fe48e85e2074c40bdd855f29b294a4
Steve Zimmerman <szimmerman@berklee.edu>
2014-11-10 11:57:50 -0500

    light and dark

--- js/property_proto.js
+++ js/property_proto.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
   // load the includes
   $( "header#main" ).load( "includes/header.html" );
   $( "#carousel" ).load( "includes/carousel.html" );
+  $( "#love-and-know" ).load( "includes/love-and-know.html" );
+  $( "#coltype-reviews-ratings" ).load( "includes/coltype-reviews-ratings.html" );
   $( "#amenities" ).load( "includes/amenities.html" );
   $( ".search-form" ).load( "includes/search-form.html" );
   $( ".room-selector-placeholder" ).load( "includes/room-list.html" );
