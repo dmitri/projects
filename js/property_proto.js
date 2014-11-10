186f99dda5139448c4b640dbdb3e65bd26d7a23a
Steve Zimmerman <szimmerman@berklee.edu>
2014-11-10 04:55:46 -0500

    latest drafting of page

--- js/property_proto.js
+++ js/property_proto.js
@@ -2,9 +2,16 @@
 
 $( document ).ready(function() {
   // load the includes
-  $( "#carousel" ).load( "carousel.html" );
-  // pause the carousel
-  //$('.carousel').pause();
+  $( "header#main" ).load( "includes/header.html" );
+  $( "#carousel" ).load( "includes/carousel.html" );
+  $( "#amenities" ).load( "includes/amenities.html" );
+  $( ".search-form" ).load( "includes/search-form.html" );
+  $( ".room-selector-placeholder" ).load( "includes/room-list.html" );
+  $( "#info-tabs" ).load( "includes/info-tabs.html" );
+  $( "#footer" ).load( "includes/footer.html" );
   
+  setTimeout(function() {
+    $("#grace-wrap").fadeIn();
+  }, 1000);
   
 });
