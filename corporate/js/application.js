9d9558c8852d10ae2f35acce3b324d9928e21fac
Steve Zimmerman <steve.zimmerman@gmail.com>
2015-03-26 01:02:59 -0400

    masthead and company work

--- corporate/js/application.js
+++ corporate/js/application.js
@@ -13,7 +13,7 @@ $( document ).ready(function() {
 
   setEvents = function() {
   
-    // set up the main nav dropdown behavior #############/
+    // set up the main nav dropdown behavior 
     // open
     $('#mainnav-drop').on('show.bs.dropdown', function () {
       toggleNavTrigger();
@@ -22,8 +22,12 @@ $( document ).ready(function() {
     $('#mainnav-drop').on('hide.bs.dropdown', function () {
       toggleNavTrigger();
     });
-
+		
+		// lazy load the images for better performance
     $("img").unveil();
+		
+		// set up the masthead behavior (shadow, shrink, grow)
+		setMastheadBehavior();
 
   
   }
@@ -35,4 +39,16 @@ toggleNavTrigger = function() {
   $('.nav-toggle').toggleClass('on');
   $('.nav-toggle i').toggleClass('fa-bars').toggleClass('fa-times');
   $('html, body').toggleClass('no-scroll');
+}
+
+setMastheadBehavior = function() {
+	$masthead = $('header#main');
+	$(window).scroll(function() {
+		// add a light shadow if not at the top
+		$(this).scrollTop() == 0 ? $masthead.removeClass('shadow') : $masthead.addClass('shadow');
+		// shrink the masthead a bit if scrolling
+		if($(window).width() > 979) {
+			$(this).scrollTop() > 100 ? $masthead.addClass('shrink') : $masthead.removeClass('shrink');
+		} 
+	});
 }
