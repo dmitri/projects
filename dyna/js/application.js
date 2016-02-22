4bfb9f8cd39fd714a7fceb8ec985b824dfea47b8
Steve Zimmerman <steve.zimmerman@gmail.com>
2016-02-22 08:46:20 -0500

    dyna proto

---
+++ dyna/js/application.js
@@ -0,0 +1,4 @@
+$(window).load(function(){
+  $("#masthead").sticky({ topSpacing: 0, className:"prime-fixed" });
+  $("#secondary-nav").sticky({ topSpacing: 0, center:true, className:"secondary-fixed" });
+});
