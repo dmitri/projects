bd599e7f9c44166f40a176bec5b65f57e11fe970
Steve Zimmerman <steve.zimmerman@gmail.com>
2016-02-22 17:21:08 -0500

    nav work

--- dyna/js/application.js
+++ dyna/js/application.js
@@ -1,12 +1,13 @@
-$(window).load(function(){
-  $("#masthead").sticky({ topSpacing: 0, className:"prime-fixed" });
-  $("#secondary-nav").sticky({ topSpacing: 0, center:true, className:"secondary-fixed" });
-  $("#tertiary-nav-container").sticky({ topSpacing:65, center:true, className:"tertiary-fixed" });
-});
-
-
-
 $( document ).ready(function() {
+
+  $('#secondary-nav-container').affix({
+    offset: {
+      top: $('#secondary-nav').offset().top-65,
+      bottom: function () {
+      }
+    }
+  });
+
   $( ".secondary-nav-toggle" ).on( "click", function(e) {
     e.preventDefault();
     $('html,body').animate({ scrollTop: 0 }, 'slow');
@@ -17,14 +18,27 @@ $( document ).ready(function() {
     $drawer_nav.slideToggle();
   });
   
+  $("#secondary-nav-container").on('affix.bs.affix', function(){
+    $('#slot-2').addClass('add-pad');
+  });
+  
+  $("#secondary-nav-container").on('affix-top.bs.affix', function(){
+    $('#slot-2').removeClass('add-pad');
+  });
+  
+  
   $( ".tertiary-tech-toggle" ).on( "click", function(e) {
-    e.preventDefault();
     
     toggle_caret(this);
 
-    $drawer_nav = $('#tertiary-nav-container');
-    $drawer_nav.slideToggle();
   });
+    
+  
+  //$("#masthead").sticky({ topSpacing: 0, className:"prime-fixed" });
+  //$("#secondary-nav").sticky({ topSpacing: 0, center:true, className:"secondary-fixed" });
+  //$("#tertiary-nav-container").sticky({ topSpacing:65, center:true, className:"tertiary-fixed" });
+  
+
 
 });
 
