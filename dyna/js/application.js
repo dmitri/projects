0670993fd1777b60210eb7c7d64b24cb573bbac6
Steve Zimmerman <steve.zimmerman@gmail.com>
2016-02-22 09:25:41 -0500

    tertiary nav

--- dyna/js/application.js
+++ dyna/js/application.js
@@ -1,4 +1,16 @@
 $(window).load(function(){
   $("#masthead").sticky({ topSpacing: 0, className:"prime-fixed" });
   $("#secondary-nav").sticky({ topSpacing: 0, center:true, className:"secondary-fixed" });
+  $("#tertiary-nav-container").sticky({ topSpacing:65, center:true, className:"tertiary-fixed" });
+});
+
+
+
+$( document ).ready(function() {
+  $( ".secondary-nav-toggle" ).on( "click", function(e) {
+    e.preventDeault();
+
+    $drawer_nav = $('#nav-drawer');
+    $drawer_nav.slideToggle();
+  });
 });
