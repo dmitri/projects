f2392d1f0a63eecc56ea6a888362421a603124ca
Steve Zimmerman <steve.zimmerman@gmail.com>
2017-02-04 16:19:27 -0500

    reorganization

---
+++ tripadvisor/js/my-app.js
@@ -0,0 +1,104 @@
+// Initialize your app
+var myApp = new Framework7({
+    // Default title for modals
+    modalTitle: 'My App',
+ 
+    // If it is webapp, we can enable hash navigation:
+    pushState: true,
+ 
+    // Hide and show indicator during ajax requests
+    onAjaxStart: function (xhr) {
+        myApp.showIndicator();
+    },
+    onAjaxComplete: function (xhr) {
+        myApp.hideIndicator();
+    },
+    
+    //Tell Framework7 to compile templates on app init
+    precompileTemplates: true,
+    
+    template7Pages: true,
+    
+//Specify templates/pages data
+    template7Data: {
+        // Plain data object
+        'page:ta-list.html': {
+            'frontend': [
+                {
+                    name:'JavaScript',
+                    description: 'Dynamic computer programming language[5]. It is most commonly used as part of web browsers, whose implementations allow...'
+                },
+                {
+                    name:'CSS',
+                    description: 'Style sheet language used for describing the look and formatting of a document written in a markup language...'
+                },
+            ],
+            'backend': [
+                {
+                    name: 'PHP',
+                    description: 'Server-side scripting language designed for web development but also used as a general-purpose programming language...'
+                },
+                {
+                    name: 'Ruby',
+                    description: 'Dynamic, reflective, object-oriented, general-purpose programming language...'
+                }
+            ]
+        }
+    }
+});  
+
+// Render person template to HTML, its template is already compiled and accessible as Template7.templates.personTemplate
+var hotelData = Template7.templates.taList({
+    name: 'John Doe',
+    age: 33,
+    position: 'Developer',
+    company: 'Apple'
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
+
+
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
