06f08a9a14c362a791251ede1a3ac9f117b376ea
Steve Zimmerman <steve.zimmerman@gmail.com>
2017-11-27 00:44:40 -0500

    first commit of the start

---
+++ fresenius/app/js/application.js
@@ -0,0 +1,8 @@
+// $( document ).ready(function() {
+//   $.getJSON("../data/assets.json",function(data){
+//       $.each(data,function(index,element){
+//           console.log(element.IT_OWNER);
+//       });
+//   });
+// });
+
